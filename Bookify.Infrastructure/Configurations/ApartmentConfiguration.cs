ef239b4920271c763d475f3fc0c3d9ae9bff849d
UrosVel97 <velickovic97@gmail.com>
2025-12-05 15:10:32 +0100

    Add Bookify.Infrastructure project and EF Core setup
    Added a new `Bookify.Infrastructure` project to the solution, targeting .NET 8.0. Introduced `ApplicationDbContext` for EF Core integration with PostgreSQL, using snake_case naming conventions. Registered infrastructure services via `DependencyInjection` class, including `IDateTimeProvider` and `IEmailService`.
    
    Configured entities (`Apartment`, `Booking`, `User`) with their respective configurations, mapping value objects and enforcing constraints. Added unique index on `User.Email`. Implemented `DateTimeProvider` and a no-op `EmailService`. Added necessary package references and project dependencies.

---
+++ Bookify.Infrastructure/Configurations/ApartmentConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Configurations
+{
+    internal sealed class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+    {
+        public void Configure(EntityTypeBuilder<Apartment> builder)
+        {
+            builder.ToTable("apartments");
+            //defining a primary key
+            builder.HasKey(apartment => apartment.Id);
+            //Mapping out Address value object to the corresponding columns in the apartments table
+            builder.OwnsOne(apartment => apartment.Address);
+
+            //Mapping the Name value object to the corresponding columns in the apartments table and
+            //putting constraints on the corresponding columns in the table. We are also converting the
+            //Name property to primitive type in the database, also converting the primitive type from 
+            //the columns to a Name value object.
+            builder.Property(apartment => apartment.Name)
+                .HasMaxLength(200)
+                .HasConversion(name=> name.Value, value => new Name(value));
+
+            builder.Property(apartment => apartment.Description)
+                .HasMaxLength(2000)
+                .HasConversion(description => description.Value, value => new Description(value));
+
+            builder.OwnsOne(apartment => apartment.Price, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+            builder.OwnsOne(apartment => apartment.CleaningFee, priceBuilder =>
+            {
+                priceBuilder.Property(money => money.Currency)
+                    .HasConversion(currency => currency.Code, code => Currency.FromCode(code));
+            });
+
+
+
+
+        }
+    }
+}
